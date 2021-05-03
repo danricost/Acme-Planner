f1f870dad4eac30d279b696066f40146acda7b0a
Daniel Rico Ostos <danricost@alum.us.es>
2021-05-03 18:57:38 +0200

    pasamos a nivel C

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
@@ -14,12 +14,10 @@ package acme.features.anonymous.shout;
 
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import acme.entities.customization.Customization;
 import acme.entities.shouts.Shout;
 import acme.framework.repositories.AbstractRepository;
 
@@ -29,9 +27,4 @@ public interface AnonymousShoutRepository extends AbstractRepository {
 	@Query("select s from Shout s where s.moment >= ?1 order by s.moment DESC ")
 	Collection<Shout> findMany(Date antiguedad);
 
-
-	@Query("select c from Customization c")
-	List<Customization> findCustomization();
-	
-	
 }
